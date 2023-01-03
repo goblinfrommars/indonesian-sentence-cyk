71f45a14338f57f8139cd7ad82df2583394d2934
gedeapriana <gedeapriana36@gmail.com>
2023-01-04 07:24:42 +0800

    finishing 🏁

--- view/front_end.py
+++ view/front_end.py
@@ -1,13 +1,11 @@
-import requests
 import streamlit as st
 import controller.cnf_conversion.after_conversion as grammar
-# from streamlit_lottie import st_lottie
 import controller.cyk_algorithm.execution as cyk
 import controller.cnf_conversion.cfg as cfg
 
+
 def start_fe():
   title = 'Parsing Sintaksis Kalimat Bahasa Indonesia Menggunakan Algoritma CYK'
-  # lottie = load_lottie_url("https://assets7.lottiefiles.com/temp/lf20_zLc23z.json")
   st.set_page_config(page_title='CYK Algorithm', page_icon='::tada::', layout='wide', menu_items={
     'About': f"""
       ### {title}
@@ -16,13 +14,17 @@ def start_fe():
       GitHub: https://github.com/gedeapriana/indonesian-sentence-cyk
       """
   })
-  # st_lottie(lottie, height=300, key='coding')
+
   st.write("<h2 style='text-align: center;'>Aplikasi Pengecekan Kalimat Bahasa Indonesia</h2>", unsafe_allow_html=True)
   st.write("<h5 style='text-align: center;'> GitHub: <a href='https://github.com/gedeapriana/indonesian-sentence-cyk'>gedeapriana/indonesian-sentence-cyk</a></h2>", unsafe_allow_html=True)
   st.write('')
 
   final_dic = grammar.get_grammar()
+
   cfg.display(final_dic)
+
+  write_cfg = open("controller/cnf_conversion/cnf-all.txt", "w")
+  write_cfg.writelines(set(final_dic))
 
   with st.container():
     st.write('---')
@@ -41,9 +43,3 @@ def start_fe():
     with rule_column:
       st.write('### CNF Rules:')
       st.write(final_dic)
-
-def load_lottie_url(url):
-  req = requests.get(url)
-  if req.status_code != 200:
-    return None
-  return req.json()
